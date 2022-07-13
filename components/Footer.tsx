36db9953c909001c0a36105860957528ef99ece1
MinJunKim21 <jinju8012@gmail.com>
2022-07-14 00:00:11 +0900

    footer 단락들 위치 수정

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,40 +1,42 @@
 function Footer() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
-      <div className="space-y-4 text-xs text-gray-800">
-        <h5 className="font-bold">ABOUT</h5>
-        <p>How Airbnb works</p>
-        <p>Newsroom</p>
-        <p>Investors</p>
-        <p>Airbnb Plus</p>
-        <p>Airbnb Luxe</p>
-      </div>
+    <div className="bg-gray-100">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14  text-gray-600 max-w-7xl mx-auto ">
+        <div className="space-y-4 text-xs text-gray-800">
+          <h5 className="font-bold">ABOUT</h5>
+          <p>How Airbnb works</p>
+          <p>Newsroom</p>
+          <p>Investors</p>
+          <p>Airbnb Plus</p>
+          <p>Airbnb Luxe</p>
+        </div>
 
-      <div className="space-y-4 text-xs text-gray-800">
-        <h5 className="font-bold">COMMUNITY</h5>
-        <p>Accessibility</p>
-        <p>This is not a real site</p>
-        <p>Its a pretty awesome clone</p>
-        <p>Referrals accepted</p>
-        <p>Minjun Kim</p>
-      </div>
+        <div className="space-y-4 text-xs text-gray-800">
+          <h5 className="font-bold">COMMUNITY</h5>
+          <p>Accessibility</p>
+          <p>This is not a real site</p>
+          <p>Its a pretty awesome clone</p>
+          <p>Referrals accepted</p>
+          <p>Minjun Kim</p>
+        </div>
 
-      <div className="space-y-4 text-xs text-gray-800">
-        <h5 className="font-bold">HOST</h5>
-        <p>Kim</p>
-        <p>Presents</p>
-        <p>Frontend</p>
-        <p>Developing</p>
-        <p>To you</p>
-      </div>
+        <div className="space-y-4 text-xs text-gray-800">
+          <h5 className="font-bold">HOST</h5>
+          <p>Kim</p>
+          <p>Presents</p>
+          <p>Frontend</p>
+          <p>Developing</p>
+          <p>To you</p>
+        </div>
 
-      <div className="space-y-4 text-xs text-gray-800">
-        <h5 className="font-bold">SUPPORT</h5>
-        <p>Help Centre</p>
-        <p>Trust & Safety</p>
-        <p>Always open</p>
-        <p>What to help</p>
-        <p>For visitors</p>
+        <div className="space-y-4 text-xs text-gray-800">
+          <h5 className="font-bold">SUPPORT</h5>
+          <p>Help Centre</p>
+          <p>Trust & Safety</p>
+          <p>Always open</p>
+          <p>What to help</p>
+          <p>For visitors</p>
+        </div>
       </div>
     </div>
   );
